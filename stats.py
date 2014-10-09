f6478a9f5f181d6a6b20152379de1cf4f869f653
William Hanwoody <hanwoody@gmail.com>
2014-10-09 15:15:48 +0800

    display more cpu stats

--- stats.py
+++ stats.py
@@ -1,32 +1,14 @@
 #!/usr/bin/env python
 
+# name: system stats
+# desc: CPU,mem,disk,network stats for linux
+# lang: python
 
 import os
 import time
 import subprocess
 from subprocess import Popen, PIPE
 
-
-def cpu_percents(sample_duration=1):
-    deltas = __cpu_time_deltas(sample_duration)
-    total = sum(deltas)
-    percents = [100 - (100 * (float(total - x) / total)) for x in deltas]
-
-    return {
-        'user': percents[0],
-        'nice': percents[1],
-        'system': percents[2],
-        'idle': percents[3],
-        'iowait': percents[4],
-        'irq': percents[5],
-        'softirq': percents[6],
-    }
-
-def procs_running():
-    return __proc_stat('procs_running')
-
-def procs_blocked():
-    return __proc_stat('procs_blocked')
 
 def file_desc():
     with open('/proc/sys/fs/file-nr') as f:
@@ -44,22 +26,43 @@ def load_avg():
     
     return load_avgs
 
-def __cpu_time_deltas(sample_duration):
+def cpu_stats(sample_duration=2):
     with open('/proc/stat') as f1:
         with open('/proc/stat') as f2:
-            line1 = f1.readline()
+            c1 = f1.read()
             time.sleep(sample_duration)
-            line2 = f2.readline()
-    
-    deltas = [int(b) - int(a) for a, b in zip(line1.split()[1:], line2.split()[1:])]
-    
-    return deltas
-    
-def __proc_stat(stat):
-    with open('/proc/stat') as f:
-        for line in f:
-            if line.startswith(stat):
-                return int(line.split()[1])
+            c2 = f2.read()
+
+    cs1 = {}
+    for l in c1.splitlines():
+        if 'cpu' not in l:
+            continue
+        d = l.strip().split()
+        cs1[d[0]] = d[1:]
+
+    cs2 = {}
+    for l in c2.splitlines():
+        if 'cpu' not in l:
+            continue
+        d = l.strip().split()
+        cs2[d[0]] = d[1:]
+
+    cs = {}
+    for c in cs1.keys():
+        deltas = [int(b) - int(a) for a, b in zip(cs1[c], cs2[c])]
+        total = sum(deltas)
+        percents = [100 - (100 * (float(total - x) / total)) for x in deltas ]
+        cs[c] = {
+            'user': percents[0],
+            'nice': percents[1],
+            'system': percents[2],
+            'idle': percents[3],
+            'iowait': percents[4],
+            'irq': percents[5],
+            'softirq': percents[6],
+        }
+
+    return cs
 
 
 def disk_usage():
@@ -81,7 +84,8 @@ def disk_stats(sample_duration=2):
             content1 = f1.read()
             time.sleep(sample_duration)
             content2 = f2.read()
-    ds = ds1 = ds2 = {}
+
+    ds1 = {}
     for l in content1.splitlines():
         if 'loop' in l:
             continue
@@ -89,6 +93,8 @@ def disk_stats(sample_duration=2):
             continue
         d = l.strip().split()
         ds1[d[2]] = [d[3], d[7], d[4], d[8], d[12]]
+
+    ds2 = {}
     for l in content2.splitlines():
         if 'loop' in l:
             continue
@@ -97,6 +103,7 @@ def disk_stats(sample_duration=2):
         d = l.strip().split()
         ds2[d[2]] = [d[3], d[7], d[4], d[8], d[12]]
 
+    ds = {}
     for d in ds1.keys():
         rnum = float(int(ds2[d][0]) - int(ds1[d][0])) / sample_duration
         wnum = float(int(ds2[d][1]) - int(ds1[d][1])) / sample_duration
@@ -139,7 +146,6 @@ def net_stats(sample_duration=2):
             content2 = f2.read()
     sep = ':'
     stats1 = {}
-    stats2 = {}
     for line in content1.splitlines():
         if sep in line:
             i = line.split(':')[0].strip()
@@ -147,6 +153,8 @@ def net_stats(sample_duration=2):
             rx_bytes1, tx_bytes1 = (int(data[0]), int(data[8]))
             rx_pack1, tx_pack1 = (int(data[1]), int(data[9]))
             stats1[i] = [rx_bytes1, tx_bytes1, rx_pack1, tx_pack1]
+
+    stats2 = {}
     for line in content2.splitlines():
         if sep in line:
             i = line.split(':')[0].strip()
@@ -169,24 +177,27 @@ class NetError(Exception):
     pass
 
 
-""" example usage of linux-metrics """
-
 def main():
     
     # load
     print 'load: %s' % load_avg()[0]
     
     # cpu
-    print "\ncpu stats:\n%14s %8s %8s %8s %8s %8s %8s" % ("user", "nice", "system", "idle", "iowait", "irq", "softirq")
-    cs = cpu_percents(sample_duration=2)
-    print "%14.1f %8.1f %8.1f %8.1f %8.1f %8.1f %8.1f" % (cs['user'], cs['nice'], cs['system'], \
-          cs['idle'], cs['iowait'], cs['irq'], cs['softirq'])
+    print "\ncpu stats:\n%14s %8s %8s %8s %8s %8s %8s %8s" % ("cpu", "user", "nice", "system", "iowait", "irq", "softirq", "idle")
+    cs = cpu_stats()
+    cpus = cs.keys()
+    cpus.sort()
+    for c in cpus:
+      print "%14s %7.1f%% %7.1f%% %7.1f%% %7.1f%% %7.1f%% %7.1f%% %7.1f%%" % (c, cs[c]['user'], \
+          cs[c]['nice'], cs[c]['system'], cs[c]['iowait'], cs[c]['irq'], cs[c]['softirq'], cs[c]['idle'])
 
     # disk
-    print '\ndisk usage:\n%14s %8s %8s %8s %8s %s' % ("device", "total(MB)", "used(MB)", "free(MB)", "used%", "mount point")
+    print '\ndisk usage:\n%14s %9s %8s %8s %8s %s' % ("device", "total(MB)", "used(MB)", "free(MB)", "used%", "mount point")
     df = disk_usage()
-    for d in df.keys():
-      print '%14s %8s %8s %8s %8s %s' % (d, df[d][0], df[d][1], df[d][2], df[d][3], df[d][4])
+    disks = df.keys()
+    disks.sort()
+    for d in disks:
+      print '%14s %9s %8s %8s %8s %s' % (d, df[d][0], df[d][1], df[d][2], df[d][3], df[d][4])
 
     print "\ndisk stats:\n%14s %8s %8s %8s %8s %8s" % ("device", "rKB/s", "wKB/s", "r/s", "w/s", "util%")
     ds = disk_stats()
@@ -196,17 +207,19 @@ def main():
       print "%14s %8.1f %8.1f %8.1f %8.1f %7s%%" % (d, ds[d][0], ds[d][1], ds[d][2], ds[d][3], ds[d][4])
       
     # memory
-    print "\nmem stats:\n%14s %8s %8s %8s %8s" % ("total", "used", "cached", "free", "usage%")
+    print "\nmem stats:\n%14s %8s %8s %8s %8s" % ("total(MB)", "used(MB)", "cached(MB)", "free(MB)", "usage%")
     used, total, cached, free, _, _ = mem_stats()
     mem_usage = float(used) * 100 / float(total)
-    print "%14s %8s %8s %8s %7.2f%%" % (int(total)/1048576, int(used)/1048576, \
+    print "%14s %8s %10s %8s %7.2f%%" % (int(total)/1048576, int(used)/1048576, \
       int(cached)/1048576, int(free)/1048576, mem_usage)
 
     # network
     print "\nnetwork stats:\n%14s %8s %8s %8s %8s" % ("interface", "rbyte/s", "tbyte/s", "rpps", "tpps")
     nss = net_stats()
-    for i in nss.keys():
-      print "%14s %8s %8s %8s %8s" % (i, nss[i][0], nss[i][1], nss[i][2], nss[i][3])
+    interfaces = nss.keys()
+    interfaces.sort()
+    for i in interfaces:
+      print "%14s %8d %8d %8d %8d" % (i, nss[i][0], nss[i][1], nss[i][2], nss[i][3])
     
     
 if __name__ == '__main__':   
