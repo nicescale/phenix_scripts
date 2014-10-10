a3b1f738f8c1bcd5d8f7518c0ac0a82259e23934
William Hanwoody <hanwoody@gmail.com>
2014-10-10 19:55:52 +0800

    fix ssl key

--- deploy_nicescale_phenix.sh
+++ deploy_nicescale_phenix.sh
@@ -11,10 +11,12 @@ JOBWORKER_PORT=9997
 KEYSERVER_PORT=9998
 API_PORT=9999
 
-keyname=phenix_$RANDOM
-ssh-keygen -b 2048 -t rsa -f /tmp/$keyname -q -N ""
-[ -f $PUBKEY_PATH ] || mv /tmp/$keyname $PUBKEY_PATH
-[ -f $PRIVKEY_PATH ] || mv /tmp/$keyname $PRIVKEY_PATH
+#keyname=phenix_$RANDOM
+#ssh-keygen -b 2048 -t rsa -f /tmp/$keyname -q -N ""
+openssl genrsa -out /tmp/private.pem 2048
+openssl rsa -in /tmp/private.pem -outform PEM -pubout -out /tmp/public.pem
+[ -f $PUBKEY_PATH ] || mv /tmp/public.pem $PUBKEY_PATH
+[ -f $PRIVKEY_PATH ] || mv /tmp/private.pem $PRIVKEY_PATH
 
 # pull docker image
 docker pull docker.nicescale.com:5000/nicescale/mongodb:latest
@@ -35,6 +37,8 @@ docker run -d -v $PRIVKEY_PATH:/tmp/jobworker.pem:ro --name jobworker -p $JOBWOR
 
 docker run -d --name=phenix-keys -v /data/phenix/keys:/data -p $KEYSERVER_PORT:80 -e KEY_SERVER_DATA_DIR=/data nicescale/phenix-keyserver
 
-docker run -d --name=phenix-api -e KEY_SERVER_URL=http://127.0.0.1:$KEYSERVER_PORT -e JOB_WORKER_URL=http://127.0.0.1:$JOBWORKER_PORT -e DB_NAME=phenix --link=phenix-mongodb:mongodb -p $API_PORT:80 nicescale/phenix-api
+IP=$(/sbin/ip ad|grep -P 'inet \d+\.\d+\.\d+\.\d+'|cut -f1 -d'/'|awk '{print $2}'|grep -v 127.0.0.1|grep -v -P '\d+\.\d+\.\d+\.1$'|head -1)
 
-docker run -d --link phenix-api:api -p 80:80 nicescale/phenix-www 
+docker run -d --name=phenix-api -e KEY_SERVER_URL=http://$IP:$KEYSERVER_PORT -e JOB_WORKER_URL=http://$IP:$JOBWORKER_PORT -e DB_NAME=phenix --link=phenix-mongodb:mongodb -p $API_PORT:80 nicescale/phenix-api
+
+docker run -d --name phenix-console --link phenix-api:api -p 80:80 nicescale/phenix-www 
