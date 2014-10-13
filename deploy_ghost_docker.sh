6efc6c9cd53675e6bae16c558091140f60429d0b
William Hanwoody <hanwoody@gmail.com>
2014-10-13 11:46:55 +0800

    fix config.js

--- deploy_ghost_docker.sh
+++ deploy_ghost_docker.sh
@@ -1,4 +1,6 @@
-#!/bin/sh
+#!/bin/bash
+
+set -e
 
 docker pull smalldocker/ghost:latest
 
@@ -10,8 +12,18 @@ docker run --rm --name $tmp_name smalldocker/ghost sleep 10 &
 
 sleep 2
 
-docker cp $tmp_name:/ghost/config.example.js $HOME/ghost/config.js
+docker cp $tmp_name:/ghost/config.example.js $HOME/ghost/
 
-docker run -d -v $HOME/ghost:/ghost-override -p 80:2368 smalldocker/ghost
+mv $HOME/ghost/config.example.js $HOME/ghost/config.js
 
-echo "> you can custom config.js in $HOME/ghost/"
+sed -i -e 's/127.0.0.1/0.0.0.0/' $HOME/ghost/config.js
+
+DOCKER_NAME=myghost$RANDOM
+
+docker ps --all | grep -q $DOCKER_NAME && docker rm $DOCKER_NAME
+
+docker run -d --name=$DOCKER_NAME -v $HOME/ghost:/ghost-override -p 80:2368 smalldocker/ghost
+
+echo "> ghost docker running, open your ghost blog in browser http://ip:80"
+
+echo "> you can custom config.js in $HOME/ghost/, then run: docker restart $DOCKER_NAME"
