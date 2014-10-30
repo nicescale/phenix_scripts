cd6812efaf96c8782aa6a319f306dffe03b7ec5f
William Hanwoody <hanwoody@gmail.com>
2014-10-30 17:33:40 +0800

    compress archive cmd

--- command_examples/archive_compress/tar.sh
+++ command_examples/archive_compress/tar.sh
@@ -14,11 +14,13 @@ This Line Has All Its First Character Of The Word With Upper Case.
 Two lines above this line is empty.
 And this is the last line.
 EOF
-mkdir demo_dir
-cp demo_file demo_dir/demo_file1
-cp demo_file demo_dir/demo_file2
-cp demo_file demo_dir/demo_file3
+mkdir foo
+cp demo_file foo/demo_file1
+cp demo_file foo/demo_file2
+cp demo_file foo/demo_file3
+cp demo_file foo/demo.jpg
 cp demo_file demo_file1
+mkdir bar
 
 # clean all when exit
 clean_all() {
@@ -28,21 +30,27 @@ trap 'clean_all' ERR
 
 # begin examples
 
-# 压缩文件demo_file并替代原来的文件为demo_file.gz
-gzip demo_file
+# 创建一个无压缩的归档文件
+tar -cvf foo.tar foo/
 
-# 压缩文件demo_file但不覆盖原来的文件
-gzip -c demo_file1 > demo_file1.gz
+# 解包一个未压缩的归档文件, 选项D表示解包到指定目录，否则是解包到本目录下
+tar -xvf foo.tar
+tar -xvf foo.tar -C ./bar
 
-# 递归压缩一个目录下所有文件，且会覆盖里面的文件为filename.gz
-gzip -r demo_dir
+# 创建一个压缩归档文件,选项z对应gzip, j对应bzip2, X对应compress
+tar -czvf foo.tgz foo/
+tar -cjvf foo.tar.bz2 foo/
 
-# 解压文件demo_file.gz,并替代原来的压缩文件为demo_file
-gzip -d demo_file.gz
+# 解压一个压缩归档文件,选项z对应gzip,j对应bzip2,X对应compress
+tar -xzvf foo.tgz
+tar -xjvf foo.tar.bz2
 
-# 解压文件demo_file1.gz，但不覆盖原来的压缩文件
-gzip -d -c demo_file1.gz > demo_file2
-zcat demo_file1.gz > demo_file3
+# 列出压缩归档文件里的文件列表
+tar -ztvf foo.tgz
+tar -jtvf foo.tar.bz2
+
+# 创建一个tgz压缩文件，但jpg,gif等文件除外
+tar czvf foo.tgz --exclude=\*.{jpg,gif,png,wmv,flv,tar.gz,zip} foo/
 
 # end examples
 ls -lR $sandbox_dir
--- command_examples/archive_compress/zip.sh
+++ command_examples/archive_compress/zip.sh
@@ -23,6 +23,8 @@ cp demo_file demo_dir/demo_file3
 cp demo_file demo_file1
 cp demo_file demo_file2
 cp demo_file demo_file3
+cp demo_file demo.sh
+cp demo_file demo1.sh
 
 # clean all when exit
 clean_all() {
@@ -35,19 +37,27 @@ trap 'clean_all' ERR
 # 将文件demo_file demo_file1和目录demo_dir一起打包压缩成demo.zip，原来的文件仍然存在，-r表示会递归目录进行压缩
 zip -r demo.zip demo_file demo_file1 demo_dir
 
-exit
-# 将文件添加到压缩文件中去
-zip demo.zip demo_file2 demo_file3
+# 查看zip压缩包中的文件
+unzip -lv demo.zip
 
-# 递归压缩一个目录下所有文件，且会覆盖里面的文件为filename.gz
-gzip -r demo_dir
+# 找出本目录下的shell文件并全部压缩为shell.zip. -@表示从标准输入接收文件列表
+find . -name "*.sh" -print | zip shell -@
 
-# 解压文件demo_file.gz,并替代原来的压缩文件为demo_file
-gzip -d demo_file.gz
+# 从标准输入接收压缩数据，生成shell.zip文件
+cat demo*.sh | zip shell -
 
-# 解压文件demo_file1.gz，但不覆盖原来的压缩文件
-gzip -d -c demo_file1.gz > demo_file2
-zcat demo_file1.gz > demo_file3
+# 将本目录压缩直接通过网络传输到备份中心，本地不生成zip文件
+zip -r - . | ssh foo@backuphost.com "cat > backup.zip"
+zip -r - . | ssh foo@backuphost.com dd of=backup.zip
+
+# 除了demo_file3外，其余文件都压缩
+zip -r demo_file.zip . -x demo_file3
+
+# 将压缩包中的demo_file1删除
+zip -d demo_file.zip demo_file1
+
+# 将压缩包的文件解压到特定目录下去
+unzip demo_file.zip -d $sandbox_dir/testdir
 
 # end examples
 ls -lR $sandbox_dir
